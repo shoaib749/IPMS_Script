ddf121d7aa6d169639588772a57bf31c36203b79
Shoaib Ansari <66304849+shoaib749@users.noreply.github.com>
2022-03-23 11:16:02 +0530

    driveDate function added

---
+++ v1/internshipStartEnd.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../includes/dbOpreations.php';
+$response = array();
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+   if(isset($_POST['email_id'])){
+         $db = new dbOpreations;
+        $result = $db->getInternshipDate($_POST['email_id']);
+        // $response['error'] = false;
+        $response = $result;
+   }else{
+        $response['error'] = true;
+        $response['message'] = "Required Fields are missing";
+   }
+   
+}else{
+        $response['error'] = true;
+        $response['message'] = "Invalid Request" ;
+}
+echo json_encode($response);
