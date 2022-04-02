3d570cd967bde74a242ee2a7473b95018335b85c
Shoaib Ansari <66304849+shoaib749@users.noreply.github.com>
2022-04-02 15:08:03 +0530

    Update allCurrentDrive.php

--- v1/allCurrentDrive.php
+++ v1/allCurrentDrive.php
@@ -2,16 +2,10 @@
 require_once '../includes/dbOpreations.php';
 $response = array();
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+$db = new dbOpreations();
+$company = $db->getAllDrive();
+$response['error'] = false;
+$response = $company;
 
-        $db = new dbOpreations();
-        $company = $db->getAllDrive();
-        $response['error'] = false;
-        $response = $company;
-   
 
-}else{
-$response['error'] = true;
-$response['message'] = "Invalid Request" ;
-}
 echo json_encode($response);
